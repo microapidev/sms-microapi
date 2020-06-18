56182ae28d9e7c85d7546e5cf7caf9f7989d3b84
poojithamiryala <poojithamiryala@gmail.com>
2020-06-18 19:59:40 +0530

    save recipient details and getting all recipents

--- smsApp/models.py
+++ smsApp/models.py
@@ -3,6 +3,6 @@ import uuid
 # Create your models here.
 class Receipent(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100, unique=False)
+    name = models.CharField(max_length=100, unique=True)
     email = models.CharField(unique=True, max_length=50, null=True)
     phone_number = models.CharField(unique=True, null=True, blank=False, max_length=100)
--- smsApp/urls.py
+++ smsApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import create_receipents_details
+from .views import create_receipents_details, get_recipient_details, save_recipients_details
 
 urlpatterns = [
-    path('v1/sms/receipent/create', create_receipents_details),
+    path('v1/sms/recipient/create', create_receipents_details),
+    path('v1/sms/recipient/save', save_recipients_details),
+    path('v1/sms/recipient/all', get_recipient_details)
 ]
