2a470db9db60e978bd8192c053a417e233883298
Kenenna Onwuagba <onwuagbakenenna@gmail.com>
2020-06-22 01:42:09 +0100

    Revert "Docker added"

--- smsApi/settings.py
+++ smsApi/settings.py
@@ -40,16 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
-    'smsApp',
-=======
     'rest_framework',
     'rest_framework_swagger',
     'smsApp',
     
 
     #'smsApp',
->>>>>>> 18e6164a73ab7268ad46441dc6ceee65eb7f03ab
     'rest_framework',
     'rest_framework_swagger',
 ]
@@ -138,6 +134,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
