ef54af41ccb81f30034675ea89f5e5012af455f1
poojithamiryala <poojithamiryala@gmail.com>
2020-06-26 20:52:55 +0530

    established rabbitmq connection,declared queues,added listeners inoder to consume message , given example of how to publish messages,consume messages

--- smsApp/apps.py
+++ smsApp/apps.py
@@ -1,5 +1,14 @@
+import threading
+
 from django.apps import AppConfig
+
+from smsApp.amqp import AMQPConsuming
 
 
 class SmsappConfig(AppConfig):
     name = 'smsApp'
+
+    def ready(self):
+        consumer = AMQPConsuming()
+        consumer.daemon = True
+        consumer.start()
