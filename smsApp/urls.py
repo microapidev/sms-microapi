74c4f9282d04f7add1bc72e3a68c114883511f69
fairtraxx <fairtraxx@protonmail.com>
2020-06-26 20:11:59 +0200

    Implemented new Swagger generator

--- smsApp/urls.py
+++ smsApp/urls.py
@@ -3,8 +3,24 @@ from django.urls import path
 from .views import create_receipents_details, get_recipient_details, save_recipients_details
 from rest_framework.schemas.coreapi import AutoSchema
 from rest_framework_swagger.views import get_swagger_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-doc_view = get_swagger_view(title="SMS API documentation")
+#doc_view = get_swagger_view(title="SMS API documentation") #This generator is no longer used
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="SMS API",
+      default_version='v1',
+      description="Test the endpoints of miroapi's SMS API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('user/',userdetails),
@@ -13,6 +29,9 @@ urlpatterns = [
     path('v1/sms/recipient/save', save_recipients_details),
     path('v1/sms/recipient/all', get_recipient_details),
     path('v1/sms/message/translate', translateMessages),
-    # path('doc/', doc_view),
-    path('', doc_view),
+    #path('doc/', doc_view), #oldswagger generator endpoint
+    #path('', doc_view), #old swagger generator endpoint
+    #path('swagger(P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'), #not used for now
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
