be17ab6bc7745aca9023a7bef187e500c98b3888
onwuagba <onwuagbakenenna@gmail.com>
2020-06-30 00:38:32 +0100

    update requirements.txt

--- smsApi/settings.py
+++ smsApi/settings.py
@@ -155,6 +155,8 @@ PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 # FORCE_SCRIPT_NAME = '/smsApp'
 # STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
