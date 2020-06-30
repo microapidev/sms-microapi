e12e6b91a75b2ed7e86c5dbbd9e35d5c69516e6c
Kenenna Onwuagba <onwuagbakenenna@gmail.com>
2020-06-30 23:51:38 +0100

    Revert "New develop branch"

--- smsApi/urls.py
+++ smsApi/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('smsApp.urls')),
     # path('', include('sms_api_interface.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
