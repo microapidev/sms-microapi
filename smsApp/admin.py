27106b0645accd15f24bdb9aaeec7aa8bd51f518
onwuagba <onwuagbakenenna@gmail.com>
2020-07-01 00:06:37 +0100

    update branch

--- smsApp/admin.py
+++ smsApp/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Receipent, User
+# from .models import Receipent, User
+from .models import Receipent
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("phoneNumber", "name", "email", "is_active", "service")
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ("phoneNumber", "name", "email", "is_active", "service")
 
 
-admin.site.register(Receipent)
-admin.site.register(User, UserAdmin)
+# admin.site.register(Receipent)
+# admin.site.register(User, UserAdmin)
