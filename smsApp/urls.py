9f6745180dc7292313d709d4ab211443ab24265b
Philbrainy <philbrainy@gmail.com>
2020-07-07 08:00:17 -0700

    Response Format

--- smsApp/urls.py
+++ smsApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import NuobjectsSendMessage, NuobjectsGetBalance, NuobjectsMessageList
 from .views import ReceipientCreate, ReceipientList, RecipientDetail
-from .views import InfobipSendMessage, InfobipSingleMessage, InfobipMessageList
+from .views import InfobipSendMessage, InfobipSingleMessage, InfobipMessageList, InfobipGroupMessage
 from .views import GroupList, GroupBySenderList, GroupDetail, GroupCreate, GroupNumbersList,GroupNumbersCreate, GroupNumbersDetail
 from .views import sendmessage, translateMessages, nuobj_api, SmsHistoryList, SmsHistoryDetail #sendmessage_infobip, get_recipients_ibp
 from .views import send_group_twilio, TwilioSendSms
@@ -53,6 +53,7 @@ urlpatterns = [
    
    #Infobip Views
    path("v1/sms/infobip/send_sms", InfobipSendMessage.as_view(), name="infobip-send-message"),
+   path("v1/sms/infobip/send_group_sms", InfobipGroupMessage.as_view(), name="infobip-group-message"),
    path("v1/sms/infobip/view_all_sms", InfobipMessageList.as_view(), name="infobip-sent-messages"),
    path("v1/sms/infobip/view_all_sms/<str:senderID>", InfobipSingleMessage.as_view(), name="infobip-sent-messages"),
 
