7a41cafcf954035b473ec0d037995fa0b42f5dfc
Kenenna Onwuagba <onwuagbakenenna@gmail.com>
2020-07-18 13:21:28 +0100

    Update settings.py

--- smsApi/settings.py
+++ smsApi/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 #SECRET_KEY = os.environ.get('SECRET_KEY', "sdjsdshhskdj")
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']  ##allows all hosts
 
